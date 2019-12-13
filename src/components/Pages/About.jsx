4f1576ed6c0cdd85a1245c3e0592eb0e01f51771
bombnp <bomb.NP@gmail.com>
2019-12-13 15:10:14 +0700

    Wrap up

--- src/components/Pages/About.jsx
+++ src/components/Pages/About.jsx
@@ -24,7 +24,7 @@ const About = (props) => {
         <Image src="/images/about/myself.png" size="small" floated="left" />
         <h3>Hi there.</h3>
         <p>My name is <span style={{fontWeight: "bold"}}>Nut Pinyo</span>. I'm a first-year student from Chulalongkorn University and a self-taught backend developer.</p>
-        <p>I believe that you can be good at anything, as long as you're passionate about it. I studied backend development by myself with some help from seniors from the university. With that, I developed APIs for <a target="_blank" rel="noopener noreferrer" href="https://projectstack.now.sh/">projectstack.now.sh</a>. </p>
+        <p>I believe that you can be good at anything, as long as you're passionate about it. I studied backend development by myself with some help from seniors from the university. With that, I developed the API for <a target="_blank" rel="noopener noreferrer" href="https://projectstack.now.sh/">projectstack.now.sh</a>. </p>
         <p>I also have some interests in frontend development. I created this website in my free time to practice making a website using React as well as to display myself to other people.</p>
         <p>If you have any questions, or if you wish to contact me, feel free to <a href="#" onClick={() => props.changeTab(5)}>contact</a> me right away.</p>
       </Container>
